aba29aedd121363e2073563d44dc1e2b74ab488f
valcor27 <valle_corona@outlook.com>
2026-02-03 10:16:25 -0600

    Subida de archivos de laravel y bootstrap

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'Inicio')
+
+@section('content')
+    <h1 class="text-primary">¡Bootstrap funcionando!</h1>
+
+    <button class="btn btn-primary">
+        Botón con mi paleta
+    </button>
+    <button class="btn btn-outline-primary">
+        Botón con mi paleta
+    </button>
+
+    <button class="btn btn-secondary">
+        Botón con mi paleta
+    </button>
+
+    <button class="btn btn-outline-secondary">
+        Botón con mi paleta
+    </button>
+@endsection
